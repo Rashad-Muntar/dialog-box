5f7a1ca8194fc446a454ea0a9ee0e916ef62bbde
Rashad Muntar <muntartoure@gmail.com>
2022-10-28 21:45:26 +0000

    implment open modal funcitonality

---
+++ index.js
@@ -0,0 +1,7 @@
+let modal = document.getElementById("modal");
+var btn = document.getElementById("openModal");
+
+btn.onclick = function() {
+    modal.style.display = "block";
+    console.log("testing  modal")
+  }
